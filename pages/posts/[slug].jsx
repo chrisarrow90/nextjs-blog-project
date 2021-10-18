d0a74b61a3aee656d92f0afc2a1d6a3e48606d4f
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-10-18 17:02:01 +1300

    create page file structure

---
+++ pages/posts/[slug].jsx
@@ -0,0 +1,9 @@
+const PostDetailPage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default PostDetailPage
---
+++ pages/posts/index.jsx
@@ -0,0 +1,9 @@
+const AllPostsPage = () => {
+  return (
+    <div>
+      <h1>All Posts</h1>
+    </div>
+  )
+}
+
+export default AllPostsPage

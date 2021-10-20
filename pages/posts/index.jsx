2b83b447aadf9edbd298945d14ab6698983a6b6d
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-10-20 17:48:37 +1300

    add all posts component

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,9 +1,38 @@
+import AllPosts from '../../components/posts/all-posts'
+
+const DUMMY_POSTS = [
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs'
+  },
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs2'
+  },
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs3'
+  },
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs4'
+  }
+]
+
 const AllPostsPage = () => {
-  return (
-    <div>
-      <h1>All Posts</h1>
-    </div>
-  )
+  return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostsPage

d26f5be290402e8c95d7ad908c478535e0238591
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-10-20 18:45:09 +1300

    update post dynamic route to use staticprops with markdown data

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,7 +1,30 @@
+import { getPostData, getPostsFiles } from '../../lib/posts-util'
 import PostContent from '../../components/posts/post-detail/post-content'
 
-const PostDetailPage = () => {
-  return <PostContent />
+const PostDetailPage = ({ post }) => {
+  return <PostContent post={post} />
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params
+  const post = getPostData(slug)
+
+  return { props: { post }, revalidate: 600 }
+}
+
+export async function getStaticPaths() {
+  const postFilenames = getPostsFiles()
+
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ''))
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug
+      }
+    })),
+    fallback: false
+  }
 }
 
 export default PostDetailPage
